6fa43fc77feb2bfb19084f698cfe15dda10b971e
Ascalon <ascalon0823@gmail.com>
2022-09-05 20:35:10 +0800

    Add square grid helper function

--- Runtime/SquareGrid.cs
+++ Runtime/SquareGrid.cs
@@ -30,7 +30,20 @@ namespace CGS.Grid
                 {new Vector2Int(-1, 0), SquareDir.Left},
                 {new Vector2Int(-1, 1), SquareDir.UpLeft},
             };
-
+        private static readonly Dictionary<int, SquareDir> IntCoordMap =
+            new Dictionary<int, SquareDir>
+            {
+                {-1, SquareDir.UpLeft},
+                {0, SquareDir.Up},
+                {1, SquareDir.UpRight},
+                {2, SquareDir.Right},
+                {3, SquareDir.DownRight},
+                {4, SquareDir.Down},
+                {5, SquareDir.DownLeft},
+                {6, SquareDir.Left},
+                {7, SquareDir.UpLeft},
+                {8, SquareDir.Up},
+            };
         public static Vector2Int ToCoord(this SquareDir squareDir)
         {
             return squareDir switch
@@ -46,10 +59,12 @@ namespace CGS.Grid
                 _ => throw new ArgumentOutOfRangeException(nameof(squareDir), squareDir, null)
             };
         }
+
         public static Vector3 To2DVector3(this SquareDir squareDir)
         {
-            return (Vector2)squareDir.ToCoord();
+            return (Vector2) squareDir.ToCoord();
         }
+
         public static Vector3 ToVector3(this SquareDir squareDir)
         {
             return squareDir switch
@@ -82,6 +97,17 @@ namespace CGS.Grid
             };
         }
 
+        public static SquareDir Next(this SquareDir squareDir)
+        {
+            return IntCoordMap[(int) squareDir + 1];
+        }
+        
+        public static SquareDir Prev(this SquareDir squareDir)
+        {
+            return IntCoordMap[(int) squareDir - 1];
+        }
+
+
         public static SquareDir GetDir(Vector2Int dir)
         {
             return DirCoordToEnum[dir];
@@ -89,11 +115,18 @@ namespace CGS.Grid
 
         public static Vector2Int[] Neighbours(this Vector2Int pos, int depth = 1)
         {
-            var result = new Vector2Int[depth*8];
-            
+            var result = new Vector2Int[depth * 8];
+            for (var i = 0; i < 8; i++)
+            {
+                for (var j = 0; j < depth; j++)
+                {
+                    result[i * depth + j] = pos + ((SquareDir) i).ToCoord() * depth + ((SquareDir) i).ToCoord() * j;
+                }
+            }
+
             return result;
         }
-        
+
 
         public static Vector2Int Neighbour(this Vector2Int pos, SquareDir dir, int depth = 1)
         {
@@ -108,7 +141,7 @@ namespace CGS.Grid
 
     public class SquareGrid<T> : SpatialGrid<T, Vector2Int>
     {
-        public SquareGrid(int x, int y, Vector3 anchorPos,Vector3 cellSize) : base(x, y, anchorPos,
+        public SquareGrid(int x, int y, Vector3 anchorPos, Vector3 cellSize) : base(x, y, anchorPos,
             cellSize)
         {
         }
@@ -124,7 +157,7 @@ namespace CGS.Grid
 
         public override Vector3 GetPos(Vector2Int coord)
         {
-            return AnchorPos + new Vector3(coord.x * CellSize.x, 0,coord.y * CellSize.z) + HalfCellSize;
+            return AnchorPos + new Vector3(coord.x * CellSize.x, 0, coord.y * CellSize.z) + HalfCellSize;
         }
 
         public override T GetCell(Vector2Int coord)
@@ -139,11 +172,12 @@ namespace CGS.Grid
 
         public override Vector2Int FromPos(Vector3 pos)
         {
-            return new Vector2Int(Mathf.FloorToInt(pos.x-AnchorPos.x), Mathf.FloorToInt(pos.z-AnchorPos.z));
+            return new Vector2Int(Mathf.FloorToInt(pos.x - AnchorPos.x), Mathf.FloorToInt(pos.z - AnchorPos.z));
         }
+
         public override bool IsValid(Vector2Int coord)
         {
-            return IsValid(coord.x,coord.y);
+            return IsValid(coord.x, coord.y);
         }
     }
 }
